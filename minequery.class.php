b5f1269a3cc379ecb5feb4499949010a1a30360c
Kramer Campbell <kramer@kramerc.com>
2010-12-26 17:14:10 -0800

    Fixed up the code. Now works on Windows servers.

--- minequery.class.php
+++ minequery.class.php
@@ -2,50 +2,43 @@
 /**
  * Query a Minecraft server running hMod with Minequery
  */
-class Minequery
-{
+class Minequery {
+	public $query;
 	
-	function __construct($server, $port = 25566)
-	{
+	public function __construct($server, $port = 25566) {
 		$this->socket = fsockopen($server, $port, $erno, $erst, 5);
 		$this->query();
 	}
 	
-	private function query()
-	{
-		$query = "QUERY" . PHP_EOL ;
+	private function query() {
+		$query = "QUERY\n";
 		$buffer = "";
 		fwrite($this->socket, $query);
 		while (!feof($this->socket)) {
 			$buffer .= fgets($this->socket, 1024);
 		}
-		$this->query = explode(PHP_EOL, $buffer);
+		$this->query = explode("\n", $buffer);
 	}
 	
-	public function port()
-	{
+	public function port() {
 		$port = explode(' ', $this->query[0]);
 		return $port[1];
 	}
 	
-	public function player_count()
-	{
+	public function player_count() {
 		$count = explode(' ', $this->query[1]);
 		return $count[1];
 	}
 	
-	public function max_players()
-	{
+	public function max_players() {
 		$max = explode(' ', $this->query[2]);
 		return $max[1];
 	}
 	
-	public function player_list()
-	{
+	public function player_list() {
 		$list = explode(' ', $this->query[3]);
 		$list = trim($list[1], '[]');
 		$list = explode(',', $list);
 		return $list;
 	}
-	
 }
