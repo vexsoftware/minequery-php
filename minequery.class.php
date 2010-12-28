197161c939f44636ab7a13b93a1a954f87ba25f8
Ben Phelps <bphelpsen@gmail.com>
2010-12-28 12:00:06 -0800

    I guess this would be version 2, sorry about not posting this to you faster.

--- minequery.class.php
+++ minequery.class.php
@@ -1,44 +1,87 @@
 <?php
+
 /**
- * Query a Minecraft server running hMod with Minequery
- */
-class Minequery {
-	public $query;
+* Query a Minecraft server running hMod with Minequery
+*/
+
+// new line constant
+define("NL", "\n");
+
+class Minequery
+{
 	
-	public function __construct($server, $port = 25566) {
+	// create the socket and establish a connection
+	// with the minecraft server
+	function __construct($server, $port = 25566)
+	{
 		$this->socket = fsockopen($server, $port, $erno, $erst, 5);
-		$this->query();
+		if ($this->socket == FALSE) {
+			// error out and die if we cant connect
+			trigger_error("Could not connect to the remote server", E_USER_ERROR);
+			die();
+		}
+		else {
+			$this->query();
+		}
+		
 	}
 	
-	private function query() {
-		$query = "QUERY\n";
+	// send the request and store the result for later
+	// processing by the class
+	private function query()
+	{
+		$query = "QUERY" . NL ;
 		$buffer = "";
 		fwrite($this->socket, $query);
 		while (!feof($this->socket)) {
 			$buffer .= fgets($this->socket, 1024);
 		}
-		$this->query = explode("\n", $buffer);
+		$this->query = explode(NL, $buffer);
 	}
 	
-	public function port() {
+	
+	// these functions split the line into 2 sections 
+	// and return the second section
+	
+	// SERVERPORT ####
+	public function port()
+	{
 		$port = explode(' ', $this->query[0]);
 		return $port[1];
 	}
 	
-	public function player_count() {
+	// PLAYERCOUNT ####
+	public function player_count()
+	{
 		$count = explode(' ', $this->query[1]);
 		return $count[1];
 	}
 	
-	public function max_players() {
+	// MAXPLAYERS ####
+	public function max_players()
+	{
 		$max = explode(' ', $this->query[2]);
 		return $max[1];
 	}
 	
-	public function player_list() {
-		$list = explode(' ', $this->query[3], 2);
+	
+	// this function splits the line into two sections
+	// then trims off the [ ] then explodes it by the 
+	// "," then we loop through the results and trim
+	// any whitespace that might be left
+	
+	// PLAYERLIST [ABCD,EFGH,etc]
+	public function player_list()
+	{
+		$list = explode(' ', $this->query[3], 2 );
 		$list = trim($list[1], '[]');
-		$list = explode(', ', $list);
-		return $list;
+		$list = explode(',', $list);
+		foreach ($list as $player) {
+			$player_list[] = trim($player);
+		}
+		return $player_list;
 	}
+	
 }
+
+/* End of file minequery.class.php */
