d63db84a2e7abfd0cd7be026d760ce32b2df9b0c
Ben Phelps <bphelpsen@gmail.com>
2010-12-28 12:06:31 -0800

    First one was all wrong.

--- usage.php
+++ usage.php
@@ -1,22 +1,26 @@
 <?php
+
+// turn on error reporting so we know if something is wrong
 error_reporting(E_ALL);
 
 require('minequery.class.php');
 
-$status = new Minequery('mc1.rewiredhost.com');
+$status = new Minequery('localhost');
 
 echo "Server Port: ";
 echo $status->port();
-echo "<br />";
-
-echo "Currently Online: ";
-echo $status->player_count();
 echo "<br />";
 
 echo "Max Players: ";
 echo $status->max_players();
 echo "<br />";
 
+echo "Currently Online: ";
+echo $status->player_count();
+echo "<br />";
+
 echo "<pre>";
 print_r($status->player_list());
 echo "</pre>";
+
+/* End of usage.php */
